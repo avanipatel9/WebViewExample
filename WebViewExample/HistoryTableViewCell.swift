586625d23927e2441ab847325a7c43e2f0d6062b
Avani Patel <anpatel36@yahoo.in>
2020-03-24 20:01:03 -0400

    [AP] history logic

---
+++ WebViewExample/HistoryTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  HistoryTableViewCell.swift
+//  WebViewExample
+//
+//  Created by Avani Patel on 3/24/20.
+//  Copyright © 2020 Avani Patel. All rights reserved.
+//
+
+import UIKit
+
+class HistoryTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var lblHistoryURL: UILabel!
+    
+}
--- WebViewExample/ViewController.swift
+++ WebViewExample/ViewController.swift
@@ -7,14 +7,77 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+    let sb = UIStoryboard(name: "Main", bundle: nil)
+    var historyList:[WKBackForwardListItem] = []
+    @IBOutlet weak var myWebKitView: WKWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        loadLambtonUrl()
+        
+    }
+    
+    @IBAction func btnHistoryClck(_ sender: Any) {
+        if myWebKitView.canGoBack
+        {
+            historyList = myWebKitView.backForwardList.backList
+            if historyList.count>0
+            {
+                let historyTableVC = sb.instantiateViewController(identifier: "HistoryTableViewController") as HistoryTableViewController
+                historyTableVC.history=self.historyList; self.navigationController?.pushViewController(historyTableVC, animated: true)
+            }
+            
+        }
+        
+    }
+    func loadLambtonUrl()
+    {
+        let url = URL(string: "https://www.lambtoncollege.ca/")
+        let urlReq = URLRequest(url: url!)
+        myWebKitView.load(urlReq)
+    }
+    
+    func loadHtmlString()
+    {
+        let htmlString = "<h1>Hello World</h1>"
+        myWebKitView.loadHTMLString(htmlString, baseURL: nil)
+        
     }
 
-
+    @IBAction func btnNavigation(_ sender: UIBarButtonItem) {
+        switch sender.tag {
+        case 0://Home Button is pressed
+            myWebKitView.reloadFromOrigin()
+        case 1://Prev Button is pressed
+            if myWebKitView.canGoBack
+            {
+                print("Go back")
+                myWebKitView.goBack()
+            }else
+            {
+                print("Can't go back")
+            }
+        case 2:
+            if myWebKitView.canGoForward
+            {
+                print("Go forward")
+                myWebKitView.goForward()
+            }
+            else
+            {
+                print("Can't go forward")
+            }
+        case 3:
+            myWebKitView.reloadFromOrigin()
+            //myWebKitView.reload()
+            
+            
+        default:
+            print("No navigation action found...")
+        }
+    }
+    
 }
 
